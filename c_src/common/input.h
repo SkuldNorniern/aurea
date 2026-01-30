6a344847e25522434606965f039c76a8bafd7598
Eira <skuldnorniern@gmail.com>
2026-01-30 13:58:46 +0900

    feat: add header for input event

---
+++ c_src/common/input.h
@@ -0,0 +1,84 @@
+#ifndef NATIVE_GUI_INPUT_H
+#define NATIVE_GUI_INPUT_H
+
+#define NG_KEY_A 0u
+#define NG_KEY_B 1u
+#define NG_KEY_C 2u
+#define NG_KEY_D 3u
+#define NG_KEY_E 4u
+#define NG_KEY_F 5u
+#define NG_KEY_G 6u
+#define NG_KEY_H 7u
+#define NG_KEY_I 8u
+#define NG_KEY_J 9u
+#define NG_KEY_K 10u
+#define NG_KEY_L 11u
+#define NG_KEY_M 12u
+#define NG_KEY_N 13u
+#define NG_KEY_O 14u
+#define NG_KEY_P 15u
+#define NG_KEY_Q 16u
+#define NG_KEY_R 17u
+#define NG_KEY_S 18u
+#define NG_KEY_T 19u
+#define NG_KEY_U 20u
+#define NG_KEY_V 21u
+#define NG_KEY_W 22u
+#define NG_KEY_X 23u
+#define NG_KEY_Y 24u
+#define NG_KEY_Z 25u
+
+#define NG_KEY_0 26u
+#define NG_KEY_1 27u
+#define NG_KEY_2 28u
+#define NG_KEY_3 29u
+#define NG_KEY_4 30u
+#define NG_KEY_5 31u
+#define NG_KEY_6 32u
+#define NG_KEY_7 33u
+#define NG_KEY_8 34u
+#define NG_KEY_9 35u
+
+#define NG_KEY_SPACE 36u
+#define NG_KEY_ENTER 37u
+#define NG_KEY_ESCAPE 38u
+#define NG_KEY_TAB 39u
+#define NG_KEY_BACKSPACE 40u
+#define NG_KEY_DELETE 41u
+#define NG_KEY_INSERT 42u
+#define NG_KEY_HOME 43u
+#define NG_KEY_END 44u
+#define NG_KEY_PAGE_UP 45u
+#define NG_KEY_PAGE_DOWN 46u
+
+#define NG_KEY_UP 47u
+#define NG_KEY_DOWN 48u
+#define NG_KEY_LEFT 49u
+#define NG_KEY_RIGHT 50u
+
+#define NG_KEY_F1 51u
+#define NG_KEY_F2 52u
+#define NG_KEY_F3 53u
+#define NG_KEY_F4 54u
+#define NG_KEY_F5 55u
+#define NG_KEY_F6 56u
+#define NG_KEY_F7 57u
+#define NG_KEY_F8 58u
+#define NG_KEY_F9 59u
+#define NG_KEY_F10 60u
+#define NG_KEY_F11 61u
+#define NG_KEY_F12 62u
+
+#define NG_KEY_SHIFT 63u
+#define NG_KEY_CONTROL 64u
+#define NG_KEY_ALT 65u
+#define NG_KEY_META 66u
+
+#define NG_KEY_UNKNOWN 0xFFFFFFFFu
+
+#define NG_MOD_SHIFT 0x1u
+#define NG_MOD_CTRL 0x2u
+#define NG_MOD_ALT 0x4u
+#define NG_MOD_META 0x8u
+
+#endif // NATIVE_GUI_INPUT_H
